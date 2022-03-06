b15b048e5d860037ab88150e591d597b2c191b77
Daniel Kucera <daniel.kucera@gmail.com>
2022-03-06 20:36:30 +0100

    version 1.0

--- panelize.sh
+++ panelize.sh
@@ -1,7 +1,9 @@
-kikit panelize --layout 'grid; rows: 3; cols: 1; space: 6mm' \
+kikit panelize --layout 'grid; rows: 4; cols: 4; space: 0mm' \
 	--source 'tolerance: 15mm' \
 	--tabs 'fixed; width: 5mm' \
 	--cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.1mm' \
-	--framing 'railslr; width: 5mm; space: 3mm' \
-	--fiducials '3fid; hoffset: 2.5mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;' \
-	iec62056-optical.kicad_pcb panel.kicad_pcb 
+	--framing 'railslr; width: 3mm; space: 0.1mm' \
+	--fiducials '3fid; hoffset: 1.5mm; voffset: 1.5mm; coppersize: 2mm; opening: 1mm;' \
+	iec62056-optical.kicad_pcb panel.kicad_pcb
+
+kikit fab jlcpcb --no-drc --assembly --schematic iec62056-optical.kicad_sch panel.kicad_pcb jlcpcb
